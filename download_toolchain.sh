6babb7b33b774c09286e8024c7f10a34c0c12e58
Hiroshi Takey (hiro2233) <htakey@gmail.com>
2018-01-22 01:13:45 -0400

    root: updated script installer.

--- download_toolchain.sh
+++ download_toolchain.sh
@@ -2,11 +2,22 @@
 
 wget -c --trust-server-names -P ../archives https://sourceforge.net/projects/urus-buildroot.urus.p/files/host-linux32-i686-linux.tar.gz.md5/download
 wget -c --trust-server-names -P ../archives https://sourceforge.net/projects/urus-buildroot.urus.p/files/host-linux32-i686-linux.tar.gz/download
+wget -c --trust-server-names -P ../archives https://sourceforge.net/projects/urus-buildroot.urus.p/files/v1.0.0/wx-urus/wx-2.8-urus-msw.tar.gz.md5/download
+wget -c --trust-server-names -P ../archives https://sourceforge.net/projects/urus-buildroot.urus.p/files/v1.0.0/wx-urus/wx-2.8-urus-msw.tar.gz/download
 
 cd ../archives
 
 RETOK=`md5sum -c ./host-linux32-i686-linux.tar.gz.md5`
-RETOK=`printf "$RETOK" | grep -rwi - -e "ok" | wc -l`
+if [ `printf "$RETOK" | grep -ri - -e "linux" | wc -l` = 0 ] ; then
+    exit 127
+fi
+
+RETOK=`md5sum -c ./wx-2.8-urus-msw.tar.gz.md5`
+if [ `printf "$RETOK" | grep -ri - -e "msw" | wc -l` = 0 ] ; then
+    exit 127
+fi
+
+RETOK=1
 
 cd ../system
 
@@ -14,6 +25,7 @@ if [ $RETOK != 0 ] ; then
     printf "MD5 ok!\n"
     sleep 2
     tar -xvzf ../archives/host-linux32-i686-linux.tar.gz
+    tar -xvzf ../archives/wx-2.8-urus-msw.tar.gz
     exit 0
 else
     #rm -rf /archives
@@ -21,5 +33,5 @@ else
     printf "MD5 doesn't match.\n"
     printf "removing temp dir and stoping intallation!\n\n\n\n"
     sleep 2
-    exit 1
+    exit 127
 fi
