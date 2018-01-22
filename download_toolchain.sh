ce691ca7446b87889122037272d21d755309a04a
hiro2233 <htakey@gmail.com>
2018-01-22 22:33:10 +0000

    root: added wx-2.8-msw-urus srf url.

--- download_toolchain.sh
+++ download_toolchain.sh
@@ -2,11 +2,22 @@
 
 wget -c --trust-server-names -P /archives https://sourceforge.net/projects/urus-buildroot.urus.p/files/host-win32-i686-mingw32.tar.gz.md5/download
 wget -c --trust-server-names -P /archives https://sourceforge.net/projects/urus-buildroot.urus.p/files/host-win32-i686-mingw32.tar.gz/download
+wget -c --trust-server-names -P /archives https://sourceforge.net/projects/urus-buildroot.urus.p/files/v1.0.0/wx-urus/wx-2.8-urus-msw.tar.gz.md5/download
+wget -c --trust-server-names -P /archives https://sourceforge.net/projects/urus-buildroot.urus.p/files/v1.0.0/wx-urus/wx-2.8-urus-msw.tar.gz/download
 
 cd /archives
 
 RETOK=`md5sum -c ./host-win32-i686-mingw32.tar.gz.md5`
-RETOK=`printf "$RETOK" | grep -rwi - -e "ok" | wc -l`
+if [ `printf "$RETOK" | grep -ri - -e "mingw32" | wc -l` = 0 ] ; then
+    exit 127
+fi
+
+RETOK=`md5sum -c ./wx-2.8-urus-msw.tar.gz.md5`
+if [ `printf "$RETOK" | grep -ri - -e "msw" | wc -l` = 0 ] ; then
+    exit 127
+fi
+
+RETOK=1
 
 cd /
 
@@ -14,6 +25,7 @@ if [ $RETOK != 0 ] ; then
     printf "MD5 ok!\n"
     sleep 2
     /busybox tar -xvzf /archives/host-win32-i686-mingw32.tar.gz -C mingw32
+    /busybox tar -xvzf /archives/wx-2.8-urus-msw.tar.gz
     exit 0
 else
     #rm -rf /archives
