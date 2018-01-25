a4bf7ce3d259ddd847909131c593515aa465175b
hiro2233 <htakey@gmail.com>
2018-01-24 22:09:59 -0400

    install: udpated system path on fstab script.

--- update_fstab.sh
+++ update_fstab.sh
@@ -11,7 +11,7 @@ sh -c '
         if [ "$URUSINFSTAB" != 0 ] ; then
             printf "URUS bind are present on /etc/fstab\n"
         else
-            printf "\n$(pwd) /system/urus none bind\n" >> /etc/fstab
+            printf "\n$(pwd)/system /system/urus none bind\n" >> /etc/fstab
             sed -i "1i export PATH=/system/urus/bin:"$"PATH\n" /etc/bash.bashrc
             printf "URUS path bind INSTALLED! on /etc/fstab\n"
         fi
