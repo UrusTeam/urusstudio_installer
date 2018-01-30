f4eac9336994096288e9e0d44953097726e682d9
hiro2233 <htakey@gmail.com>
2018-01-30 06:27:39 -0400

    root: added mingw32 toolchain download.

--- download_toolchain.sh
+++ download_toolchain.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
 
-wget -c --trust-server-names -P ../archives https://sourceforge.net/projects/urus-buildroot.urus.p/files/host-linux32-i686-linux.tar.gz.md5/download
-wget -c --trust-server-names -P ../archives https://sourceforge.net/projects/urus-buildroot.urus.p/files/host-linux32-i686-linux.tar.gz/download
+#wget -c --trust-server-names -P ../archives https://sourceforge.net/projects/urus-buildroot.urus.p/files/v1.0.0/host-linux32-i686-linux.tar.gz.md5/download
+#wget -c --trust-server-names -P ../archives https://sourceforge.net/projects/urus-buildroot.urus.p/files/v1.0.0/host-linux32-i686-linux.tar.gz/download
 wget -c --trust-server-names -P ../archives https://sourceforge.net/projects/urus-buildroot.urus.p/files/v1.0.0/wx-urus/wx-2.8-urus-msw.tar.gz.md5/download
 wget -c --trust-server-names -P ../archives https://sourceforge.net/projects/urus-buildroot.urus.p/files/v1.0.0/wx-urus/wx-2.8-urus-msw.tar.gz/download
+wget -c --trust-server-names -P ../archives https://sourceforge.net/projects/urus-buildroot.urus.p/files/v1.0.0/host-linux32-i686-mingw32.tar.gz.md5/download
+wget -c --trust-server-names -P ../archives https://sourceforge.net/projects/urus-buildroot.urus.p/files/v1.0.0/host-linux32-i686-mingw32.tar.gz/download
 
 cd ../archives
 
-RETOK=`md5sum -c ./host-linux32-i686-linux.tar.gz.md5`
-if [ `printf "$RETOK" | grep -ri - -e "linux" | wc -l` = 0 ] ; then
-    exit 127
-fi
+#RETOK=`md5sum -c ./host-linux32-i686-linux.tar.gz.md5`
+#if [ `printf "$RETOK" | grep -ri - -e "linux" | wc -l` = 0 ] ; then
+#    exit 127
+#fi
 
 RETOK=`md5sum -c ./wx-2.8-urus-msw.tar.gz.md5`
 if [ `printf "$RETOK" | grep -ri - -e "msw" | wc -l` = 0 ] ; then
+    exit 127
+fi
+
+RETOK=`md5sum -c ./host-linux32-i686-mingw32.tar.gz.md5`
+if [ `printf "$RETOK" | grep -ri - -e "mingw32" | wc -l` = 0 ] ; then
     exit 127
 fi
 
@@ -24,7 +31,8 @@ cd ../system
 if [ $RETOK != 0 ] ; then
     printf "MD5 ok!\n"
     sleep 2
-    tar -xvzf ../archives/host-linux32-i686-linux.tar.gz
+    #tar -xvzf ../archives/host-linux32-i686-linux.tar.gz
+    tar -xvzf ../archives/host-linux32-i686-mingw32.tar.gz
     tar -xvzf ../archives/wx-2.8-urus-msw.tar.gz
     exit 0
 else
