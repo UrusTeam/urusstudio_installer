58e7d4dfde10d83ca717a3286f983ce407e3b5c2
Hiroshi Takey (hiro2233) <htakey@gmail.com>
2018-02-14 18:44:01 -0400

    installer: hot-linux32-i686-linux comment out.

--- download_toolchain.sh
+++ download_toolchain.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-#wget -c --trust-server-names -P ../archives https://sourceforge.net/projects/urus-buildroot.urus.p/files/v1.0.0/host-linux32-i686-linux.tar.gz.md5/download
-#wget -c --trust-server-names -P ../archives https://sourceforge.net/projects/urus-buildroot.urus.p/files/v1.0.0/host-linux32-i686-linux.tar.gz/download
-wget -c --trust-server-names -P ../archives https://sourceforge.net/projects/urus-buildroot.urus.p/files/v1.0.0/wx-urus/wx-2.8-urus-msw.tar.gz.md5/download
-wget -c --trust-server-names -P ../archives https://sourceforge.net/projects/urus-buildroot.urus.p/files/v1.0.0/wx-urus/wx-2.8-urus-msw.tar.gz/download
-wget -c --trust-server-names -P ../archives https://sourceforge.net/projects/urus-buildroot.urus.p/files/v1.0.0/host-linux32-i686-mingw32.tar.gz.md5/download
-wget -c --trust-server-names -P ../archives https://sourceforge.net/projects/urus-buildroot.urus.p/files/v1.0.0/host-linux32-i686-mingw32.tar.gz/download
+wget -c --trust-server-names --max-redirect 5 -P ../archives https://sourceforge.net/projects/urus-buildroot.urus.p/files/v1.0.0/host-linux32-i686-linux.tar.gz.md5/download
+wget -c --trust-server-names --max-redirect 5 -P ../archives https://sourceforge.net/projects/urus-buildroot.urus.p/files/v1.0.0/host-linux32-i686-linux.tar.gz/download
+wget -c --trust-server-names --max-redirect 5 -P ../archives https://sourceforge.net/projects/urus-buildroot.urus.p/files/v1.0.0/wx-urus/wx-2.8-urus-msw.tar.gz.md5/download
+wget -c --trust-server-names --max-redirect 5 -P ../archives https://sourceforge.net/projects/urus-buildroot.urus.p/files/v1.0.0/wx-urus/wx-2.8-urus-msw.tar.gz/download
+wget -c --trust-server-names --max-redirect 5 -P ../archives https://sourceforge.net/projects/urus-buildroot.urus.p/files/v1.0.0/host-linux32-i686-mingw32.tar.gz.md5/download
+wget -c --trust-server-names --max-redirect 5 -P ../archives https://sourceforge.net/projects/urus-buildroot.urus.p/files/v1.0.0/host-linux32-i686-mingw32.tar.gz/download
 
 cd ../archives
 
-#RETOK=`md5sum -c ./host-linux32-i686-linux.tar.gz.md5`
-#if [ `printf "$RETOK" | grep -ri - -e "linux" | wc -l` = 0 ] ; then
-#    exit 127
-#fi
+RETOK=`md5sum -c ./host-linux32-i686-linux.tar.gz.md5`
+if [ `printf "$RETOK" | grep -ri - -e "linux" | wc -l` = 0 ] ; then
+    exit 127
+fi
 
 RETOK=`md5sum -c ./wx-2.8-urus-msw.tar.gz.md5`
 if [ `printf "$RETOK" | grep -ri - -e "msw" | wc -l` = 0 ] ; then
@@ -31,9 +31,9 @@ cd ../system
 if [ $RETOK != 0 ] ; then
     printf "MD5 ok!\n"
     sleep 2
-    #tar -xvzf ../archives/host-linux32-i686-linux.tar.gz
-    tar -xvzf ../archives/host-linux32-i686-mingw32.tar.gz
-    tar -xvzf ../archives/wx-2.8-urus-msw.tar.gz
+    tar -xvzf ../archives/host-linux32-i686-linux.tar.gz -C /system/urus
+    tar -xvzf ../archives/host-linux32-i686-mingw32.tar.gz -C /system/urus
+    tar -xvzf ../archives/wx-2.8-urus-msw.tar.gz -C /system/urus
     exit 0
 else
     #rm -rf /archives
