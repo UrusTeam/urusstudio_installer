e6f3234c908bfdde02e8993c20f7e98ff9e8aa3d
hiro2233 <htakey@gmail.com>
2018-02-15 09:36:12 -0400

    installer: added mime update script file.

---
+++ update_mimes.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cp -rf /system/urus/share/appdata /usr/share/
+cp -rf /system/urus/share/applications /usr/share/
+cp -rf /system/urus/share/icons /usr/share/
+cp -rf /system/urus/share/mime /usr/share/
+cp -rf /system/urus/share/pixmaps /usr/share/
+
+exit 0
