8e182f17089efc0636925f2b06d1158c08ac2bcb
hiro2233 <htakey@gmail.com>
2018-02-17 16:36:36 -0400

    installer: fixed bool ret on deps script.

--- install_deps.sh
+++ install_deps.sh
@@ -5,12 +5,14 @@
 BASEPKG="automake gcc libgtk2.0-0 libgtk2.0-dev git gamin libtool autoconf libgamin0 libgamin-dev python2.7 python2.7-dev build-essential \
          libxslt1-dev libxml2-dev python-pip python-dev zlib1g-dev cmake cmake-data"
 PLATOS=`uname -a`
-if [ `printf "$PLATOS" | grep -ri - -e "ubuntu" | wc -l` = 0 ] ; then
+if [ `printf "$PLATOS" | grep -ri - -e "ubuntu" | wc -l` = 1 ] ; then
     SCOWPWRCMD='apt-get'
     YESNOCMD='-y --force-yes'
-elif [ `printf "$PLATOS" | grep -ri - -e "debian" | wc -l` = 0 ] ; then
+    echo ubuntu
+elif [ `printf "$PLATOS" | grep -ri - -e "debian" | wc -l` = 1 ] ; then
     SCOWPWRCMD='apt-get'
     YESNOARG='-y --force-yes'
+   echo debian
 fi
 
 $SCOWPWRCMD update
