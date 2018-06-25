48bb9fc265159a8cf9827bb23e460c497cfbac02
Hiroshi Takey (hiro2233) <htakey@gmail.com>
2018-06-25 07:29:44 -0400

    linux32: added g++ package missing and return on user id."

--- install_deps.sh
+++ install_deps.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+if [ `id -u` != 0 ] ; then
+    printf "\n\nPlease run with root permission!\n\n"
+    return 127
+fi
+
 ./update_fstab.sh
 
-BASEPKG="automake gcc libgtk2.0-0 libgtk2.0-dev git gamin libtool autoconf libgamin0 libgamin-dev python2.7 python2.7-dev build-essential \
+BASEPKG="automake gcc g++ libgtk2.0-0 libgtk2.0-dev git gamin libtool autoconf libgamin0 libgamin-dev python2.7 python2.7-dev build-essential \
          libxslt1-dev libxml2-dev python-pip python-dev zlib1g-dev cmake cmake-data"
 PLATOS=`uname -a`
 if [ `printf "$PLATOS" | grep -ri - -e "ubuntu" | wc -l` = 1 ] ; then
