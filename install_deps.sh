c957b3702df4fd6710693513a18e8fe66c111953
Hiroshi Takey (hiro2233) <htakey@gmail.com>
2018-07-02 10:19:24 -0400

    linux32: fixed quiet and force yes deps install.

--- install_deps.sh
+++ install_deps.sh
@@ -9,19 +9,21 @@ fi
 
 BASEPKG="automake gcc g++ libgtk2.0-0 libgtk2.0-dev git gamin libtool autoconf libgamin0 libgamin-dev python2.7 python2.7-dev build-essential \
          libxslt1-dev libxml2-dev python-pip python-dev zlib1g-dev cmake cmake-data"
+
+YESNOCMD=""
+
 PLATOS=`uname -a`
 if [ `printf "$PLATOS" | grep -ri - -e "ubuntu" | wc -l` = 1 ] ; then
-    SCOWPWRCMD='apt-get'
-    YESNOCMD='-y --force-yes'
+    SCOWPWRCMD="apt-get"
+    YESNOCMD="-qqy"
     echo ubuntu
 elif [ `printf "$PLATOS" | grep -ri - -e "debian" | wc -l` = 1 ] ; then
-    SCOWPWRCMD='apt-get'
-    YESNOARG='-y --force-yes'
+    SCOWPWRCMD="apt-get"
+    YESNOCMD="-qqy"
    echo debian
 fi
 
-$SCOWPWRCMD update
-$SCOWPWRCMD $YESNOARG install $BASEPKG
+$SCOWPWRCMD $YESNOCMD install $BASEPKG
 
 pip install --user future lxml
 pip install --user empy catkin_pkg
