a2da0efed536d7e8dd8748b3f1fdc19765cca891
Hiroshi Takey (hiro2233) <htakey@gmail.com>
2018-07-07 01:05:27 -0400

    linux32: added download android toolchain script.

--- install_linux.sh
+++ install_linux.sh
@@ -5,6 +5,8 @@ cd system
 
 ../download_toolchain.sh
 cp -f ../start_urusstudio bin/
+cp -f ../download_android_tools bin/
+mkdir -p archives
 
 echo ----------------------------------------
 echo Install done!
