b35071f241a7cabbad8ec6eba5be9b7d9b5d4a12
Hiroshi Takey (hiro2233) <htakey@gmail.com>
2018-07-11 00:54:27 -0400

    linux32: added buil targets usp for ci.

--- install_linux.sh
+++ install_linux.sh
@@ -9,6 +9,10 @@ cp -f ../download_android_tools bin/
 mkdir -p archives
 cp -rf ../archives/urusstudio archives/
 
+if [ "x$BUILD_TARGETS" != "x" ] ; then
+start_urusstudio --user-data-dir=archives/urusstudio --build $USP_PATH --target=$USP_TARGET_PATH
+fi
+
 echo ----------------------------------------
 echo Install done!
 echo ----------------------------------------
