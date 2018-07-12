28f5114bcae2660ac22a464b9a06f39d9243d278
Hiroshi Takey (hiro2233) <htakey@gmail.com>
2018-07-12 00:01:19 -0400

    linux32: added ldconfig and mv command for py files.

--- install_linux.sh
+++ install_linux.sh
@@ -8,6 +8,8 @@ cp -f ../start_urusstudio bin/
 cp -f ../download_android_tools bin/
 mkdir -p archives
 cp -rf ../archives/urusstudio archives/
+mv -f $(pwd)/i686-pc-linux-gnu/lib/*.py ../archives/
+sudo ldconfig
 
 if [ "x$BUILD_TARGETS" != "x" ] ; then
 start_urusstudio --user-data-dir=archives/urusstudio --build $USP_PATH --target=$USP_TARGET_PATH
