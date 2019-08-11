98250d07a095915bcf63000a695493e17930ce49
Hiroshi Takey F. (hiro2233) <htakey@gmail.com>
2019-08-11 05:45:24 -0400

    root: update_fstab: added urusprofile shell for sys init.

--- update_fstab.sh
+++ update_fstab.sh
@@ -8,20 +8,21 @@ if [ -e /etc/fstab ] ; then
         printf "URUS bind are present on /etc/fstab\n"
     else
         mkdir -p /system/urus
+        printf "#!/bin/sh\n" >> /etc/profile.d/urusprofile.sh
         printf "\n$(pwd)/system /system/urus none bind\n" >> /etc/fstab
-        sed -i "1i export PATH=/system/urus/bin:"$"PATH" /etc/profile
+        #sed -i "1i export PATH=/system/urus/bin:"$"PATH" /etc/profile
         #sed -i "1i export LD_LIBRARY_PATH=/system/urus/lib:"$"LD_LIBRARY_PATH" /etc/profile
-        sed -i "1i export ACLOCAL_FLAGS=-I/system/urus/share/aclocal:"$"ACLOCAL_FLAGS" /etc/profile
-        printf "\nexport ACLOCAL_FLAGS=-I/system/urus/share/aclocal:"$"ACLOCAL_FLAGS\n" >> ~/.profile
+        #sed -i "1i export ACLOCAL_FLAGS=-I/system/urus/share/aclocal:"$"ACLOCAL_FLAGS" /etc/profile
+        printf "export ACLOCAL_FLAGS=-I/system/urus/share/aclocal:"$"ACLOCAL_FLAGS\n" >> /etc/profile.d/urusprofile.sh
         #printf "export LD_LIBRARY_PATH=/system/urus/lib:"$"LD_LIBRARY_PATH\n" >> ~/.profile
-        printf "export PATH=/system/urus/bin:"$"PATH\n" >> ~/.profile
+        printf "export PATH=/system/urus/bin:"$"PATH\n" >> /etc/profile.d/urusprofile.sh
         printf "/system/urus/i686-pc-linux-gnu/lib\n" >> /etc/ld.so.conf.d/i686-urus-linux-gnu.conf
         printf "/system/urus/lib\n" >> /etc/ld.so.conf.d/i686-urus-linux-gnu.conf
         printf "URUS path bind INSTALLED! on /etc/fstab\n"
     fi
     mount /system/urus
+    . /etc/profile.d/urusprofile.sh
     . ~/.profile
-
 fi
 
 exit 0
