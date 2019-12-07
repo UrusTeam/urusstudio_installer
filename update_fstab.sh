caf6dfcab56d1b8a1b8a6a6c53773db7baef679a
Hiroshi Takey F. (hiro2233) <htakey@gmail.com>
2019-12-07 13:07:42 -0400

    win32: added export LC_ALL to C on update fstab urusprofile script.

--- update_fstab.sh
+++ update_fstab.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-busybox sh -c '
-
     if [ -e etc/fstab ] ; then
 
         export URUSINFSTAB=$(grep -sri etc/fstab -e "/system/urus" | wc -l)
@@ -11,14 +9,15 @@ busybox sh -c '
         else
             mkdir -p system/urus
             printf "\n$(pwd) /system/urus none bind,binary,posix=0,noacl,user\n" >> etc/fstab
-            printf ""P"ATH=/system/urus/mingw32/bin:"$"PATH\n" >> etc/profile.d/urusprofile.sh
-            printf ""P"ATH=/system/urus/bin:"$"PATH\n" >> etc/profile.d/urusprofile.sh
-            printf ""P"ATH=/system/urus/lib:"$"PATH\n" >> etc/profile.d/urusprofile.sh
-            printf ""P"ATH=/usr/bin:/bin:"$"PATH\n" >> etc/profile.d/urusprofile.sh
+            printf "PATH=/system/urus/mingw32/bin:"'$'"PATH\n" >> etc/profile.d/urusprofile.sh
+            printf "PATH=/system/urus/bin:"'$'"PATH\n" >> etc/profile.d/urusprofile.sh
+            printf "PATH=/system/urus/lib:"'$'"PATH\n" >> etc/profile.d/urusprofile.sh
+            printf "export PATH=/usr/bin:/bin:"'$'"PATH\n" >> etc/profile.d/urusprofile.sh
             #printf "MSYS=winsymlinks:nativestrict\n" >> etc/profile.d/urusprofile.sh
+            printf "export LC_ALL=C\n" >> etc/profile.d/urusprofile.sh
             printf "URUS path bind INSTALLED!\n"
         fi
 
-    fi'
+    fi
 
 exit 0
