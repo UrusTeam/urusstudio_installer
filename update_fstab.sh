cc2d2197b5197d63356b9572b441f46ea8833c7d
Hiroshi Takey F. (hiro2233) <htakey@gmail.com>
2019-12-07 07:44:19 -0400

    build: moved PATH envvar to urusprofile init on profile.d path.

--- update_fstab.sh
+++ update_fstab.sh
@@ -7,16 +7,16 @@ busybox sh -c '
         export URUSINFSTAB=$(grep -sri etc/fstab -e "/system/urus" | wc -l)
 
         if [ "$URUSINFSTAB" != 0 ] ; then
-            printf "URUS bind are present on /etc/fstab\n"
+            printf "URUS bind are present!\n"
         else
             mkdir -p system/urus
             printf "\n$(pwd) /system/urus none bind,binary,posix=0,noacl,user\n" >> etc/fstab
-            printf ""P"ATH=/system/urus/mingw32/bin:"$"PATH\n" >> etc/profile
-            printf ""P"ATH=/system/urus/bin:"$"PATH\n" >> etc/profile
-            printf ""P"ATH=/system/urus/lib:"$"PATH\n" >> etc/profile
-            printf ""P"ATH=/usr/bin:/bin:"$"PATH\n" >> etc/profile
-            #printf "MSYS=winsymlinks:nativestrict\n" >> etc/profile
-            printf "URUS path bind INSTALLED! on /etc/fstab\n"
+            printf ""P"ATH=/system/urus/mingw32/bin:"$"PATH\n" >> etc/profile.d/urusprofile.sh
+            printf ""P"ATH=/system/urus/bin:"$"PATH\n" >> etc/profile.d/urusprofile.sh
+            printf ""P"ATH=/system/urus/lib:"$"PATH\n" >> etc/profile.d/urusprofile.sh
+            printf ""P"ATH=/usr/bin:/bin:"$"PATH\n" >> etc/profile.d/urusprofile.sh
+            #printf "MSYS=winsymlinks:nativestrict\n" >> etc/profile.d/urusprofile.sh
+            printf "URUS path bind INSTALLED!\n"
         fi
 
     fi'
