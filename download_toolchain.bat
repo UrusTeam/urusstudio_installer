207549fefd46994bad42115031c23e72541b40e0
Hiroshi Takey F. (hiro2233) <htakey@gmail.com>
2020-12-23 21:35:29 -0400

    download toolchain: added exit gratefully.

--- download_toolchain.bat
+++ download_toolchain.bat
@@ -5,8 +5,8 @@ chdir system/
 busybox cp ../download_toolchain.sh usr/bin/
 busybox cp ../su.exe usr/bin/
 chdir usr/bin/
-su -c "./dash -c 'PATH=$(pwd):$PATH ./download_toolchain.sh'"
-printf "don't close any console window!\nplease wait until this process finish.\n"
+su -c "./dash -c 'PATH=$(pwd):$PATH && ./download_toolchain.sh' && exit 0"
+printf "don't close any console window!\nplease wait.\n"
 dash -c "rm -f /toolchain_download_ok.txt; timeout=1; while [ ! -e /toolchain_download_ok.txt ] && [ $timeout -le 600 ]; do printf '*' && sleep 10 $(( timeout=timeout+1 )); done"
 printf "\nok\nYou can close this window.\n"
 pause
